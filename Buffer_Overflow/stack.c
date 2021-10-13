ed1efdfc69e39280d881139471464e5535ffc731
Fabian Astudillo <fabian.astudillo.salinas@gmail.com>
2021-10-13 12:31:17 -0500

    Adding Buffer_Overflow

---
+++ Buffer_Overflow/stack.c
@@ -0,0 +1,33 @@
+/* This program has a buffer overflow vulnerability. */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/* Changing this size will change the layout of the stack.
+* Instructors can change this value each year, so students
+* won’t be able to use the solutions from the past. */
+#ifndef BUF_SIZE
+#define BUF_SIZE 100
+#endif
+
+int bof(char *str)
+{
+    char buffer[BUF_SIZE];
+
+    /* The following statement has a buffer overflow problem */
+    strcpy(buffer, str);
+
+    return 1;
+}
+
+int main(int argc, char **argv)
+{
+    char str[517];
+
+    int length = fread(str, sizeof(char), 517, stdin);
+    bof(str);
+
+    fprintf(stdout,"==== Returned Properly ====\n");
+    return 1;
+}
+

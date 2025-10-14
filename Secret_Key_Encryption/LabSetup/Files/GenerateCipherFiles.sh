b9d62110dea35fda3690d281435f3851734ab841
Fabián Astudillo-Salinas <fabian.astudillo.salinas@gmail.com>
2025-10-14 17:34:14 -0500

    modified:   Secret_Key_Encryption/LabSetup/Files/GenerateCipherFiles.sh

--- Secret_Key_Encryption/LabSetup/Files/GenerateCipherFiles.sh
+++ Secret_Key_Encryption/LabSetup/Files/GenerateCipherFiles.sh
@@ -1,30 +1,103 @@
 #!/bin/bash
-#
-# Script Name: GenerateCipherFiles.sh
-# Description: This files generate a different cipher file for each group in the
-#              Network Security course of the University of Cuenca.
-# Usage:       ./GenerateCipherFiles.sh
-#              Example: ./GenerateCipherFiles.sh
-# Author:      Fabian Astudillo <fabian.astudillos@ucuenca.edu.ec>
-# Date:        2024-10-24
-# Version:     1.0
-# License:     MIT License
-#
-#
-# Exit Status:
-#  0  : Success
-#  >0 : Failure
-#
 
-year="sep-2024"
-number_of_groups=10
+# Función para extraer texto de la web sin copyright
+# Parámetro: $1 - año (formato: Mar-2024, Sep-2024, etc.)
+# Retorna: nombre del archivo generado
 
-for i in $(seq 1 $number_of_groups);
-do
-    clave=`echo {a..z} | sed 's/ /\n/g' | shuf | tr '\n' ' ' | sed 's/ //g'`
-    #clave=`awk NR==1 claves-Sep2024.txt`
-    tr [:upper:] [:lower:] < textoclaro-Sep2024.txt > textoclaro-Sep2024-lower.txt
-    tr -cd '[a-z][\n][:space:]' < textoclaro-Sep2024-lower.txt > plaintext.txt
-    tr 'a-z' $clave < plaintext.txt > "textoCifradoGrupo${i}.bin"
+
+# Función para mostrar ayuda
+mostrar_ayuda() {
+    echo "Uso: $0 [opciones]"
+    echo "Opciones:"
+    echo "  -c          Crear archivos de texto cifrado"
+    echo "  -n NUM      Número de grupos (por defecto: 5)"
+    echo "  -s SEM      Semestre (ej: Mar, Sep)"
+    echo "  -y AÑO      Año (ej: 2025)"
+    echo "  -r          Eliminar archivos de texto cifrado"
+    echo "  -e          Eliminar archivo de texto claro"
+    echo "  -h          Mostrar esta ayuda"
+    exit 0
+}
+
+# Valores por defecto
+crear_cifrado=false
+number_of_groups=5
+eliminar_cifrados=false
+eliminar_claro=false
+semester=""
+year=""
+
+extraer_texto_web() {
+    local year="$1"
+    local output_file="textoclaro-${year}.txt"
+    
+    # URL de Project Gutenberg (dominio público, sin copyright)
+    local url="https://www.gutenberg.org/files/1342/1342-0.txt"
+    
+    # Descargar y procesar el texto
+    curl -s "$url" | \
+        head -n 100 | \
+        tail -n 50 | \
+        tr '[:upper:]' '[:lower:]' | \
+        perl -pe '
+            s/(\d+)/
+                my $num = $1;
+                my %digits = (
+                    "0" => "cero", "1" => "uno", "2" => "dos", "3" => "tres", "4" => "cuatro",
+                    "5" => "cinco", "6" => "seis", "7" => "siete", "8" => "ocho", "9" => "nueve"
+                );
+                $num =~ s#(\d)#$digits{$1}#ge;
+                $num;
+            /ge
+        ' | \
+        tr -cd '[a-z][\n][:space:]' | \
+        head -c 3000 > "$output_file"
+    echo "$output_file"
+}
+
+while getopts "cn:s:y:reh" opt; do
+    case $opt in
+        c) crear_cifrado=true ;;
+        n) number_of_groups="$OPTARG" ;;
+        s) semester="$OPTARG" ;;
+        y) year="$OPTARG" ;;
+        r) eliminar_cifrados=true ;;
+        e) eliminar_claro=true ;;
+        h) mostrar_ayuda ;;
+        *) mostrar_ayuda ;;
+    esac
+done
+
+# Eliminar archivo de texto claro
+if [ "$eliminar_claro" = true ]; then
+    rm -f textoclaro-*.txt claves-*.txt
+    echo "Archivo(s) de texto claro eliminado(s)."
+fi
+
+# Eliminar archivos cifrados
+if [ "$eliminar_cifrados" = true ]; then
+    rm -f textoCifradoGrupo*.bin
+    echo "Archivos de texto cifrado eliminados."
+fi
+
+# Crear archivos cifrados
+if [ "$crear_cifrado" = true ]; then
+    # Obtener semestre y año actual
+    current_month=$(date +%m)
+    current_year=$(date +%Y)
+    if [ "$current_month" -ge 3 ] && [ "$current_month" -lt 9 ]; then
+        semester="Mar"
+    else
+        semester="Sep"
+    fi
+    year="$current_year"
+    period="${semester}-${year}"
+    texto_archivo=$(extraer_texto_web "$period")
+fi
+
+echo "Archivos cifrados creados para $number_of_groups grupos."
+for i in $(seq 1 $number_of_groups); do    
+    clave=$(echo {a..z} | sed 's/ /\n/g' | shuf | tr '\n' ' ' | sed 's/ //g')
+    tr 'a-z' "$clave" < "$texto_archivo" > "textoCifradoGrupo${i}.bin"
     echo "$clave" >> "claves-${year}.txt"
 done
